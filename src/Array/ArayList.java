92d0e08052932d831f3631eb3998ad593bdbabcf
samueltexa <woolardsamuels@gmail.com>
2022-08-03 20:35:57 +0300

    Java for beginners

---
+++ src/Array/ArayList.java
@@ -0,0 +1,13 @@
+package Array;
+import java.util.ArrayList;
+public class ArayList {
+    public static void main(String[] args) {
+        //array list
+        ArrayList<Integer> marks = new ArrayList<Integer>();
+        marks.add(90);
+        marks.add(76);
+        marks.add(54);
+        for (int i = 0; i < marks.size(); i++)
+            System.out.print(marks.get(i) + " ");
+    }
+}
