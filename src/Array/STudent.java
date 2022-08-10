647d4208ef0a2769e1be0299d561fb1a0d41aa3d
samueltexa <woolardsamuels@gmail.com>
2022-08-10 17:19:34 +0300

    edits

---
+++ src/Array/STudent.java
@@ -0,0 +1,15 @@
+package Array;
+import java.util.ArrayList;
+public class STudent {
+        public static void main(String[] args){
+            ArrayList<String> STudent = new ArrayList<String>();
+            STudent stud1, stud2, stud3, stud4;
+            stud1 = new STudent();
+            stud2 = new STudent();
+            stud3 = new STudent();
+            stud4 = new STudent();
+
+        }
+        STudent.add(stud1);
+        STudent.add(stud2);
+    }
