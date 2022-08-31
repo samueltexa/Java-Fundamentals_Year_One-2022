9afe6f5d0b15053fcfafa434b8a69ceb7fd78875
samueltexa <woolardsamuels@gmail.com>
2022-08-31 15:44:06 +0300

    Interfaces

---
+++ src/INterfaces/TestInterfaces.java
@@ -0,0 +1,12 @@
+package INterfaces;
+
+public class TestInterfaces {
+    }
+    abstract class House{
+        abstract void open();
+    }
+    //create bathroom interface
+    interface Bathroom{
+        final double size = 10.0;
+        void sprinkleWater();
+    }
