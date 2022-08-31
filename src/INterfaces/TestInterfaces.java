ee2bd892b48e4aff1e54a2ce730b70d133a235ec
samueltexa <woolardsamuels@gmail.com>
2022-08-31 16:24:51 +0300

    full interface program

--- src/INterfaces/TestInterfaces.java
+++ src/INterfaces/TestInterfaces.java
@@ -1,6 +1,16 @@
 package INterfaces;
 
 public class TestInterfaces {
+    public static void main(String[] args){
+        Bang_aloHouse bang = new Bang_aloHouse();
+        bang.open();
+        bang.sprinkleWater();
+        StorageHouse storage = new StorageHouse();
+        storage.open();
+        storage.sprinkleWater();
+        HutHouse hut =  new HutHouse();
+        hut.open();
+    }
     }
     abstract class House{
         abstract void open();
@@ -14,13 +24,34 @@ public class TestInterfaces {
 
         @Override
         void open() {
-            System.out.println("Open bang-alo house");
+            System.out.println("Open bang-alo house.");
 
         }
 
         @Override
         public void sprinkleWater() {
-            System.out.println("Sprinkle water in bang_alo bathroom");
+            System.out.println("Sprinkle water in bang_alo bathroom.");
 
+        }
+    }
+    class StorageHouse extends House implements Bathroom{
+
+    @Override
+    void open() {
+        System.out.println("Open bang-alo house.");
+
+        }
+
+    @Override
+    public void sprinkleWater() {
+        System.out.println("Sprinkle water in bang_alo bathroom.");
+
+        }
+    }
+    class HutHouse extends House{
+
+        @Override
+        void open() {
+            System.out.println("Opens hut house.");
         }
     }
