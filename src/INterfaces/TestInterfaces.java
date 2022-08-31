539591a1edb3e914f6625706d0157d0bd92e4c7c
samueltexa <woolardsamuels@gmail.com>
2022-08-31 15:52:38 +0300

    changes to interface pachkage

--- src/INterfaces/TestInterfaces.java
+++ src/INterfaces/TestInterfaces.java
@@ -10,3 +10,17 @@ public class TestInterfaces {
         final double size = 10.0;
         void sprinkleWater();
     }
+    class Bang_aloHouse extends House implements Bathroom{
+
+        @Override
+        void open() {
+            System.out.println("Open bang-alo house");
+
+        }
+
+        @Override
+        public void sprinkleWater() {
+            System.out.println("Sprinkle water in bang_alo bathroom");
+
+        }
+    }
