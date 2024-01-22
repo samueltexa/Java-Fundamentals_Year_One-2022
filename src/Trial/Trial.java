9921e561f96b2e5a018d232ff621bb90e9c95037
samueltexa <woolarsamuels@gmail.com>
2024-01-22 11:23:58 +0300

    modified files

--- src/Trial/Trial.java
+++ src/Trial/Trial.java
@@ -1,16 +1,26 @@
 package Trial;
-import java.io.IOException;
-
-public class Trial extends IOException {
+import java.io.*;
+public class Trial{
     static int key;
-    static int[] array = new int[]{1,2,3};
-    public static void linearSearch(){
-            if (key == array[2]) {
-                System.out.println(key);
-            } else System.out.println(key);
-
+    static int count = 3;
+    static boolean found= false;
+    static int[] array = new int[]{1,2,3,4};
+    public static void linearSearch() throws IOException{
+        BufferedReader buf  = new BufferedReader(new InputStreamReader(System.in));
+        while (count<=30){
+            System.out.print("Enter the key you want to search for: ");
+            key = Integer.parseInt(buf.readLine());
+        for (int j : array) {
+            if (key == j) {
+                System.out.println("Key " +key+ " Found");
+                found = true;
+            }
+        }
+        if (!found)
+         System.out.println("Key "+key+" Not found");
+        }
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         linearSearch();
     }
 }
